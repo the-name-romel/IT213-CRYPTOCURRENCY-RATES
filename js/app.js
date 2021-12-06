b710296d9cf32e2b57f8cf8ec7e3e231931bffc3
the-name-romel <rommelbon100@gmail.com>
2021-12-06 17:00:08 +0800

    Reading Values from the Form

--- js/app.js
+++ js/app.js
@@ -2,3 +2,30 @@
 
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
+
+// Create the varibles
+
+const form = document.getElementById('form');
+
+
+
+
+// Add event listener
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // read currency
+    const currencySelect = document.getElementById('currency').value;
+    // read cryptocurrency
+    const cryptoCurrencySelect = document.getElementById('cryptocurrency').value;
+
+    
+    // validate that the selects have something
+    if(currencySelect === '' || cryptoCurrencySelect === '') {
+     // display an error
+     console.log('error');  
+    }else {
+        console.log('success');
+    }
+})
+
