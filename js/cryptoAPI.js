19abe3d07c3edd11ab55cf94ca5ff357c4ad6b47
the-name-romel <rommelbon100@gmail.com>
2021-12-08 16:41:28 +0800

    Update cryptoAPI.js

--- js/cryptoAPI.js
+++ js/cryptoAPI.js
@@ -1,8 +1,10 @@
+const proxyURL = "https://cors-anywhere.herokuapp.com/";
+
 class CryptoAPI {
     // Query the rest api with a currency and a cryptocurrency
     async queryAPI(currency, cryptocurrency) {
         // Query the URL
-        const url = await fetch(`https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=1&id=${cryptocurrency}&convert=${currency}&CMC_PRO_API_KEY=e9a14b0e-e0bb-4519-8cea-0f20e4dec3a5`);
+        const url = await fetch(`${proxyURL}https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=1&id=${cryptocurrency}&convert=${currency}&CMC_PRO_API_KEY=e9a14b0e-e0bb-4519-8cea-0f20e4dec3a5`);
 
         // Return as json
         const result = await url.json();
@@ -15,7 +17,7 @@ class CryptoAPI {
 
     // Get all the cryptocurrencies
     async getCryptoCurrenciesList() {
-        const url = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=e9a14b0e-e0bb-4519-8cea-0f20e4dec3a5');
+        const url = await fetch(`${proxyURL}https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=e9a14b0e-e0bb-4519-8cea-0f20e4dec3a5`);
 
         // Return this as a json
         const cryptoCurrencies = await url.json();
